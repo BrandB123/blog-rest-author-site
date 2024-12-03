483cbd1649e578e9b3a14c6b4c8f5fa9cce57202
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-12-02 23:15:13 -0600

    Implement API consumption to add user

--- src/routes/(authentication)/sign-up/+page.svelte
+++ src/routes/(authentication)/sign-up/+page.svelte
@@ -1,56 +1,99 @@
-<script>
+<script lang="ts">
+  import { goto } from '$app/navigation';
 
-    // const { name, email, password, authorStatus } = req.body
-    let firstName = $state("testing");
-    let lastName = $state("testing");
-
+    let firstName = $state("");
+    let lastName = $state("");
     let name = $derived(`${firstName} ${lastName}`);
-    let email = $state("testing");
-    let password = $state("testing");
+    let email = $state("");
+    let password = $state("");
 
-    // on submit,
-        // check inputs 
-        // post request to http://localhost:3000/api/signup
-        // if response 200, forward to sign in page
-        // else display error page and direct back to the sign up page again after 10 seconds
-    async function addUser(){
+    async function addUser(name: string, email: string, password: string){
         try {
             const response = await fetch("http://localhost:3000/api/signup", {
                 method: "POST",
                 body: JSON.stringify({
-                    name: {name},
-                    email: {email}, 
-                    password: {password}, 
+                    name,
+                    email, 
+                    password, 
                     authorStatus: true
-                })
-            })
-        } catch(err) {
+                }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            
+            if (response.status === 422){ 
+                alert("Name, Email, and Password must be provided")
+            }
 
+            if (response.status === 409){
+                alert("A user with this email already exists.")
+            }
+            
+            if (!response.ok){
+                throw new Error("Response: ", response.status);
+            }
+
+            goto('/log-in')
+
+        } catch(err) {
+            console.error(err);
+
+            if (err.name === 'TypeError'){
+                alert("Service currently unavailable. Sorry for the inconvenience. Please try again later.")
+            }
         }
     }
-    
 </script>
 
 <h1 class="text-3xl pl-4">Create an Account</h1>
 <form action="/"
     class="bg-neutral-100 w-1/4 mx-auto p-5 rounded-lg border border-neutral-500">
-    <label for="">First Name:</label>
-    <input type="text" name="" id="" bind:value={firstName}
-    class="block">
-    <label for="">Last Name:</label>
-    <input type="text" name="" id="" value={lastName}
-    class="block">
-    <label for="">Email:</label>
-    <input type="text" name="" id="" value={email}
-    class="block">
-    <label for="">Password:</label>
-    <input type="password" name="" id="" value={password}
-    class="block">
+
+    <label for="first-name">First Name:</label>
+    <input 
+        type="text"
+        id = "first-name"
+        required 
+        bind:value={firstName} 
+        class="block">
+
+    <label for="last-name">Last Name:</label>
+    <input 
+        type="text"
+        id = "last-name"
+        required 
+        bind:value={lastName} 
+        class="block">
+
+    <label for="email">Email:</label>
+    <input 
+        type="text"
+        id = "email"
+        required 
+        bind:value={email} 
+        class="block">
+
+    <label for="password">Password:</label>
+    <input 
+        type="password"
+        id = "password"
+        required 
+        minlength="8"
+        bind:value={password} 
+        class="block">
+
     <button type="submit"
-    class="p-1 border rounded-lg border-blue-600 bg-blue-500 text-neutral-200"
-    onclick={(event) => {
-        console.log(`NAME: ${name}, EMAIL: ${email}, PASSWORD: ${password}`);
-        event.preventDefault();
+        class="p-1 border rounded-lg border-blue-600 bg-blue-500 text-neutral-200"
+        onclick={(event) => {
+            event.preventDefault();
+            if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)){
+                alert("Invalid email entry.")
+            } else if (!/^(\S+)(\s*)(\S+)$/.test(name)){
+                alert("Invalid name entry. First and last name required")
+            } else {
+                addUser(name, email, password)
+            }
         }}>
         Create Account
     </button>
