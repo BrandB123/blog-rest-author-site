a975a614d39de4b16ca4f4e9a3dfc464d7f6680d
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-12-03 23:53:11 -0600

    Build page to update and delete posts

---
+++ src/routes/(app)/[postId]/+page.svelte
@@ -0,0 +1,210 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { goto } from "$app/navigation";
+  
+    let { data } = $props();
+    let post;
+    let title = $state("");
+    let message = $state("");
+    let published = $state(false);
+  
+    async function getPost(postId: number){
+        try{
+            const response = await fetch(
+                `${import.meta.env.VITE_API_URL}api/posts/${postId}/author`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${localStorage.getItem("authorjwt")}`
+                }
+            });
+        
+            if (response.status === 401){
+                goto('/log-in');
+            }
+
+            if (response.status === 403){
+                goto('/sign-up');
+            }
+        
+            if (!response.ok){
+                throw new Error("Response Status: ", response.status);
+            }
+        
+            const json = await response.json();
+            return json.post;
+
+        } catch(err){
+            console.error(err);
+        }
+    }
+
+    async function updatePost(title: string, message: string, published: boolean){
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}api/posts/${data.postId}/author`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${localStorage.getItem("authorjwt")}`
+                },
+                body: JSON.stringify({
+                    postTitle: title,
+                    postMessage: message,
+                    publishedStatus: published
+                })
+            });
+        
+            if (response.status === 401){
+                goto('/log-in');
+            }
+
+            if (response.status === 403){
+                goto('/sign-up');
+            }
+
+            if (response.status === 422){ 
+                alert("Please complete all fields.")
+            }
+
+            if (response.status === 409){
+                alert("You have already created a post with this title. Post titles must be unique.")
+            }
+        
+            if (!response.ok){
+                throw new Error("Response Status: ", response.status);
+            }
+        
+            goto("/");
+    
+        } catch(err){
+            console.error(err);
+        }
+    } 
+
+    async function deletePost(){
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}api/posts/${data.postId}/author`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${localStorage.getItem("authorjwt")}`
+                }
+            });
+
+            if (response.status === 401){
+                goto('/log-in');
+            }
+
+            if (response.status === 403){
+                goto('/sign-up');
+            }
+
+            if (!response.ok){
+                throw new Error("Response Status: ", response.status)
+            }
+
+            goto("/")
+
+        } catch(err) {
+            console.error(err);
+        }
+    }
+  
+    onMount(async () => {
+        post = await getPost(Number(data.postId))
+        title = post.title;
+        message = post.message;
+        published = post.publishedStatus;
+    })
+  
+</script>
+
+<h1 class="text-4xl pl-4 mt-4">Update Post</h1>
+<form 
+    action="/"
+    class="bg-neutral-200 m-8 p-5 rounded-xl flex flex-col border border-neutral-500 relative">
+
+    <label 
+        for="title"
+        class="text-xl mb-2">
+        Post Title:
+    </label>
+    <input 
+        type="text" 
+        id="title"
+        bind:value={title}
+        class="rounded-lg border border-neutral-400 mb-4 px-4 py-2">
+
+    <label 
+        for="message"
+        class="text-xl mb-2">
+        Post Message:
+    </label>
+    <textarea 
+        id="message" 
+        rows="10" 
+        cols="80"
+        bind:value={message}
+        class="rounded-lg border border-neutral-400 mb-4 px-3 py-2">
+    </textarea>
+
+    <div class="ml-4">
+        <input
+            type="radio"
+            id="publish"
+            name="publish-status"
+            bind:group={published}
+            value="true"
+            class="inline">
+        <label
+            for="publish"
+            class="inline">
+            Publish
+        </label>
+    </div>
+
+    <div class="ml-4">
+        <input
+            type="radio"
+            id="archive"
+            name="publish-status"
+            bind:group={published}
+            value="false"
+            class="inline">
+        <label
+            for="archive"
+            class="inline">
+            Archive
+        </label>
+    </div>
+
+    <button
+        type="submit"
+        class="border border-black rounded-xl p-1 w-1/6 mx-auto bg-blue-500 text-neutral-100 text-xl"
+        onclick={(event) => {
+            event.preventDefault();
+            console.log(`TITLE: ${title}, MESSAGE: ${message}, PUBLISHED: ${published}`)
+
+            if (title.length > 255){
+                alert("Title can only be 255 characters.")
+            } else if (message.length > 3000){
+                alert("Message can only be 3000 characters.")
+            } else if (!/^(.+)(\S+)(.+)$/.test(title) || !/^(.+)(\S+)(.+)$/.test(message)){
+                alert("Invalid title or message entry. Please try again")
+            } else {
+                updatePost(title, message, published);
+            }
+
+        }}>
+        Update
+    </button>
+
+    <button 
+        type="submit"
+        class="p-2 w-1/8 text-neutral-500 text-md absolute top-2 right-4 hover:font-semibold hover:text-neutral-800 "
+        onclick={(event) => {
+            event.preventDefault()
+            deletePost();
+        }}>
+        Delete Post
+    </button>
+</form>
